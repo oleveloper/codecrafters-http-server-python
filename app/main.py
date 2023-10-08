ac26ba119bc01325c6501f45a14c24a47a2d3983
oleveloper <oleveloper@gmail.com>
2023-10-08 18:43:38 +0900

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -3,8 +3,11 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
 
+    client_socket, address = server_socket.accept()
+    data = client_socket.recv(1024)
+
+    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
