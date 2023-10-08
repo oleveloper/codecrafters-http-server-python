8d5eca3ad7adcbfa31b99854d4cacdbd95b36215
oleveloper <oleveloper@gmail.com>
2023-10-08 19:12:24 +0900

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import socket
 
-
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     client_socket, address = server_socket.accept()
     data = client_socket.recv(1024)
+    path = str(data).split("\r\n")[0].split(' ')[1]
 
-    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    if path == '/': 
+      client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+      client_socket.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
 
 if __name__ == "__main__":
     main()
