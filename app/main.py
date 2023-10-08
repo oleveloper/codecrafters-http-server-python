9a445116281091fdbeefb4c73fbe2c0035068801
oleveloper <oleveloper@gmail.com>
2023-10-08 19:29:23 +0900

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -9,8 +9,16 @@ def main():
 
     if path == '/': 
       client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif path.startswith("/echo/"):
+      client_socket.sendall(response(path.replace("/echo/", "")).encode()) 
     else:
       client_socket.sendall(b"HTTP/1.1 404 NOT FOUND\r\n\r\n")
+
+def response(text):
+    status_line = "HTTP/1.1 200 OK"
+    content_type = "Content-Type: text/plain"
+    content_length = f"Content-Length: {len(text)}"
+    return f"{status_line}\r\n{content_type}\r\n{content_length}\r\n\r\n{text}"
 
 if __name__ == "__main__":
     main()
